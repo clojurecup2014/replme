10364a829194ba6661aeffaf300177645b52181f
Alex Weiksnar <aweiksnar@gmail.com>
2014-09-27 12:52:25 -0500

    repo loading display prototype

--- src/replme/cljs/repo_input.cljs
+++ src/replme/cljs/repo_input.cljs
@@ -1,5 +1,6 @@
 (ns replme.cljs.repo-input
-  (:use [jayq.core :only [$ on val]]))
+  (:use [jayq.core :only [$ on val slide-down slide-up html]]
+        [jayq.util :only [wait]]))
 
 (def $repo-input-form ($ :#repo-input-form))
 (def $repo-input ($ :#repo-input))
@@ -9,10 +10,18 @@
 (defn clear-input [input]
   (val input ""))
 
+(defn display-loading-info []
+  (let [loading-message ($ :#loading-message)]
+   (-> loading-message
+       (slide-down 450)
+       (html (str "loading repo " (val $repo-input) "..."))) ;; this'll become a stream of data
+   (wait 3000  (fn [] (slide-up loading-message 500))))) ;; this'll be a done fn
+
 (defn on-repo-submit [e]
   (.preventDefault e)
+  (display-loading-info)
   (clear-input $repo-input)
-  (println "call a function to handle submit here..."))
+  )
 
 (defn init []
   (on $repo-input-form :submit on-repo-submit))
