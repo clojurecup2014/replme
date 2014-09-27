00f7a057ad86a70da178d71c1c703232cfd8bd23
Edward Paget <edward@cassetteta.pe>
2014-09-26 21:09:09 -0500

    Redirect / to index.html

--- src/replme/web/routes.clj
+++ src/replme/web/routes.clj
@@ -1,12 +1,14 @@
 (ns replme.web.routes
-  (:require [compojure.core :refer [routes ANY]]
+  (:require [compojure.core :refer [routes ANY GET]]
             [compojure.route :as route]
             [compojure.handler :as handler]
+            [ring.util.response :as resp]
             [liberator.core :refer [resource]]))
 
 (defn- main-routes
   []
   (routes
+   (GET "/" [] (resp/resource-response "index.html" {:root "public"}))
    (route/resources "/")
    (route/not-found "/404.html"))) 
 
