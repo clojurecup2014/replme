470ef709e457e314adc39ef6cd2d4bdd41391afb
Edward Paget <edward@cassetteta.pe>
2014-09-26 22:31:39 -0500

    Add docker client connection component

--- src/replme/components/docker.clj
+++ src/replme/components/docker.clj
@@ -1,14 +1,22 @@
 (ns replme.components.docker
   (:require [com.stuartsierra.component :as component]
+            [docker.core :as docker]
             [clojure.tools.logging :as log]))
 
-(defrecord Docker [url connection]
+(defrecord Docker [url client]
   component/Lifecycle
   (start [component]
-    component)
+    (if (:client component) 
+      component
+      (do
+        (log/info (str "Connecting to docker server at " url))
+        (assoc component :client (docker/make-client url)))))
 
   (stop [component]
-    component))
+    (if-not (:client component)
+      component
+      (log/info "Disconnected from Docker")
+      (dissoc component :client))))
 
 (defn new-docker
   [url]
