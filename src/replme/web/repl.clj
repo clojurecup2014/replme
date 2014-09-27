391c723ee9cdfb97bac99ba7057294762fe1741e
Edward Paget <edward@cassetteta.pe>
2014-09-27 13:46:27 -0500

    Wait for nrepl container to be ready before connecting

--- src/replme/web/repl.clj
+++ src/replme/web/repl.clj
@@ -1,32 +1,25 @@
 (ns replme.web.repl
   (:require [clojure.core.async :refer [go <! >! <!! >!! chan close! go-loop]]
             [org.httpkit.server :refer [send! with-channel on-receive on-close open? websocket?]]
+            [http.async.client :as http]
             [clojure.tools.logging :as log]
             [replme.web.resp :refer :all]
             [clojure.tools.nrepl :as repl]
             [docker.container :as container]))
 
-(defn try-times*
-  [n sleep thunk]
-  (loop [n n]
-    (if-let [result (try
-                      [(thunk)]
-                      (catch Exception e
-                        (if (zero? n)
-                          (throw e)
-                          (Thread/sleep sleep))))]
-      (result 0)
-      (recur (dec n)))))
-
-(defmacro try-times-with-sleep
-  [n sleep & body]
-  `(try-times* ~n ~sleep (fn [] ~@body)))
+(def nrepl-sentinel #"server started")
 
 (defn- docker-attach
   [client id]
   (let [output (chan)
-        buffer (container/attach client id :logs true :stdout true)]
-    buffer))
+        url (str "http://" (:host client) "/containers/" id "/logs?stdout=1&follow=1")
+        http-client (http/create-client)]
+    (go
+      (log/info "Reading logs from: " url)
+      (doseq [msg (http/string (http/stream-seq http-client :get url))]
+        (log/info "Container" id "logs:" msg)
+        (>! output msg)))
+    [output http-client]))
 
 (defn- docker-cmd
   []
@@ -40,7 +33,7 @@
         id (:Id container)
         logs (docker-attach client id)]
     (container/start client id)
-    (log/info (str "Started container: " id))
+    (log/info (str "Started container:" id))
     [id logs]))
 
 (defn- docker-ip
@@ -55,32 +48,35 @@
 
 (defn- docker-repl
   [client in-chan out-chan]
-  (let [[id stdout] (start-docker client)
+  (let [[id [stdout http-client]] (start-docker client)
         ip (docker-ip client id)
-        port 8081
-        repl-conn (try-times-with-sleep 10 1000
-                                        (repl/connect :host ip :port port))]
-    (log/info (str "Connecting to docker nrepl at " ip ":" port))
-    (go-loop [command (<! in-chan)
-              client (repl/client repl-conn 1000)]
-      (->> (repl/message client {:op :eval :code command})
-           repl/response-values
-           prn-str
-           (>! out-chan))
-      (recur (<! in-chan) client))
-    id))
+        port 8081]
+    (go-loop [msg (<! stdout)]
+      (if (re-find nrepl-sentinel msg)
+          (go-loop [repl-conn (repl/connect :host ip :port port)
+                    command (<! in-chan)
+                    client (repl/client repl-conn 1000)]
+            (log/info (str "Connecting to docker nrepl at" ip ":" port))
+            (->> (repl/message client {:op :eval :code command})
+                 repl/response-values
+                 prn-str
+                 (>! out-chan))
+            (recur repl-conn (<! in-chan) client))
+        (recur (<! stdout))))
+    [id http-client]))
 
 (defn- handle-input
   [in-chan]
   (fn [data]
-    (log/info (str "Received: " data))
+    (log/info (str "Received:" data))
     (>!! in-chan data)))
 
 (defn- handle-close
-  [client id & chans]
+  [client id http-client & chans]
   (fn [_]
     (log/info "Closing Repl Connnection")
     (stop-docker client id)
+    (http/close http-client)
     (doseq [c chans] (close! c))))
 
 (defn- handle-out
@@ -96,9 +92,9 @@
         out-chan (chan)]
     (with-channel req channel
       (if (websocket? channel)
-        (let [docker-id (docker-repl docker-client in-chan out-chan)
+        (let [[docker-id http-client] (docker-repl docker-client in-chan out-chan)
               send-chan (handle-out channel out-chan)]
           (log/info "Websocket Connected")
           (on-receive channel (handle-input in-chan))
-          (on-close channel (handle-close docker-client docker-id in-chan out-chan send-chan)))
+          (on-close channel (handle-close docker-client docker-id http-client in-chan out-chan send-chan)))
         (send! channel (resp-bad-request))))))
