b90ebd79ce3a3aa85fda6a95197391d5b9e5ddfa
Alex Weiksnar <aweiksnar@gmail.com>
2014-09-27 16:42:55 -0500

    connect loading spinner to socket done

--- src/replme/cljs/loading_mesage.cljs
+++ src/replme/cljs/loading_mesage.cljs
@@ -1,7 +1,7 @@
 (ns replme.cljs.loading-message
   (:require [cljs.core.async :refer [<! >! chan sub]]
             [replme.cljs.websocket :as ws])
-  (:use [jayq.core :only [$]])
+  (:use [jayq.core :only [$ fade-out]])
   (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
 (enable-console-print!)
@@ -9,7 +9,8 @@
 (def $loading-message ($ :#loading-message))
 
 (defn init [socket-out-pub]
-  (let [socket-out-sub (sub socket-out-pub :socket-out (chan))]
+  (let [socket-out-sub (sub socket-out-pub :socket-out (chan))
+        loading-spinner ($ :#clojure-spinner)]
     (go-loop [msg (<! socket-out-sub)]
-             (.log js/console msg)
+             (when (= msg "REPL OK") (fade-out loading-spinner 500))
              (recur (<! socket-out-sub)))))
