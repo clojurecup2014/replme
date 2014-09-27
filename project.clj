02d9e913c3f7401977ffa1a90bc3b61fdcc086c0
Edward Paget <edward@cassetteta.pe>
2014-09-26 19:48:29 -0500

    Add Backend Deps

--- project.clj
+++ project.clj
@@ -6,6 +6,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [http-kit "2.1.16"]
+                 [compojure "1.1.9"]
+                 [liberator "0.12.2"]
+                 [clj-http "1.0.0"]
+                 [org.clojure/tools.nrepl "0.2.5"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/clojurescript "0.0-2311"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
