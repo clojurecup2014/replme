7aa20a4ad77c8dc02ca69459a2536b18d4e4ba37
Alex Weiksnar <aweiksnar@gmail.com>
2014-09-27 00:08:45 -0500

    move jquery console code to its own ns

--- src/replme/cljs/main.cljs
+++ src/replme/cljs/main.cljs
@@ -1,27 +1,6 @@
 (ns replme.cljs.main
-  (:use [jayq.core :only [$ css html document-ready]]))
+  (:use [jayq.core :only [document-ready]])
+  (:require [replme.cljs.repl :as repl]))
 
-(def $repl-container ($ :#repl-container))
-
-(enable-console-print!)
-
-(defn handle-command [input report]
-  (let [line input]
-    ;; evaluate line here and return that the as the value of "msg"...
-    (array (js-obj
-            "msg" line
-            "className" "jquery-console-message-value"))))
-
-(def repl-config (js-obj "promptLabel" ">>"
-                         "autofocus" true
-                         "welcomeMessage" "Welcome! Type in some clojure to get started"
-                         "animateScroll" true
-                         "commandHandle" handle-command
-                         "promptHistory" true))
-
-(defn setup-console []
-  (.console $repl-container repl-config))
-
-(println repl-config)
-
-(document-ready (setup-console))
+(document-ready
+  (repl/init))
---
+++ src/replme/cljs/repl.cljs
@@ -0,0 +1,25 @@
+(ns replme.cljs.repl
+  (:use [jayq.core :only [$ css html document-ready]]))
+
+(def $repl-container ($ :#repl-container))
+
+(enable-console-print!)
+
+(defn handle-command [input report]
+  (let [line input]
+    ;; evaluate line here and return that the as the value of "msg"...
+    (array (js-obj
+            "msg" line
+            "className" "jquery-console-message-value"))))
+
+
+
+(def repl-config (js-obj "promptLabel" ">>"
+                         "autofocus" true
+                         "welcomeMessage" "Welcome! Type in some clojure to get started"
+                         "animateScroll" true
+                         "commandHandle" handle-command
+                         "promptHistory" true))
+
+(defn init []
+  (.console $repl-container repl-config))
