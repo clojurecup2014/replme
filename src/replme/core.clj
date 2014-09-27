55021af5c1ad0a071ad74fd011c52fba046ebf6a
Edward Paget <edward@cassetteta.pe>
2014-09-26 19:09:17 -0500

    Lein Project Init

---
+++ src/replme/core.clj
@@ -0,0 +1,6 @@
+(ns replme.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/replme/core_test.clj
@@ -0,0 +1,7 @@
+(ns replme.core-test
+  (:require [clojure.test :refer :all]
+            [replme.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
