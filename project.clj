21b4ffce9b84f8c1f536dcaa91052ef7fd2b6a7c
Edward Paget <edward@cassetteta.pe>
2014-09-28 18:26:15 -0500

    Working advanced compilation

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject replme "0.1.0-SNAPSHOT"
-  :description "repl me"
+  :description "REPLME"
   :url "replme.clojurecup.com"
 
   :license {:name "GNU Affero General Public License v3"
             :url "https://www.gnu.org/licenses/agpl-3.0.html"}
+
+
 
   :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
                  [http-kit "2.1.18"]
@@ -16,24 +18,31 @@
                  [com.stuartsierra/component "0.2.2"]
                  [org.clojure/tools.nrepl "0.2.5"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/clojurescript "0.0-2342"]
                  [markdown-clj "0.9.53"]
                  [jayq "2.5.2"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
   :profiles {:uberjar {:main replme.core
-                       :aot [replme.core]}
-
+                       :aot [replme.core]
+                       :hooks [leiningen.cljsbuild]}
              :dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.3"]]}}
   :cljsbuild {
-              :builds [{:id "replme"
-                        :source-paths ["src"]
-                        :externs ["resources/public/externs/jquery.js",
-                                  "resources/public/externs/jquery_console.js"]
-                        :compiler {
-                                   :output-to "resources/public/replme.js"
-                                   :output-dir "resources/public/out"
-                                   :optimizations :none
-                                   :source-map true}}]})
+             :builds {:prod {:source-paths ["src"]
+                              :compiler {:preamble ["libs/jquery-1.10.2.min.js"
+                                                    "libs/jquery.console.js"]
+                                         :externs ["libs/jquery-1.10.2.min.js"
+                                                   "libs/jquery.console.js"]
+                                         :output-to "resources/public/replme.js"
+                                         :output-dir "resources/public/out"
+                                         :pretty-print false
+                                         :optimizations :advanced
+                                         :closure-warnings {:externs-validation :off
+                                                            :non-standard-jsdoc :off}}}
+                       :dev {:source-paths ["src"]
+                             :compiler {
+                                        :output-to "resources/public/replme.js"
+                                        :optimizations :none
+                                        :source-map true}}}})
