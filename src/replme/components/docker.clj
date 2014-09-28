9a09eeba6777f12120a50ad16737c4aa1bbdf4a6
Edward Paget <edward@cassetteta.pe>
2014-09-27 20:34:09 -0500

    Remove old containers on startup

--- src/replme/components/docker.clj
+++ src/replme/components/docker.clj
@@ -1,6 +1,7 @@
 (ns replme.components.docker
   (:require [com.stuartsierra.component :as component]
             [docker.core :as docker]
+            [docker.container :as container]
             [clojure.tools.logging :as log]))
 
 (defrecord Docker [url client]
@@ -9,7 +10,10 @@
     (if (:client component) 
       component
       (do (log/info (str "Connecting to docker server at " url))
-          (assoc component :client (docker/make-client url)))))
+          (let [client (docker/make-client url)]
+            (doseq [old-container (map :Id (container/show-all client))]
+              (container/remove client old-container :force true))
+            (assoc component :client client)))))
 
   (stop [component]
     (if-not (:client component)
